dbe470e230b351533158c9d92ad49171304fb3b6
aghogho <aghoghomonorien@gmail.com>
2024-04-22 09:26:44 +0100

    Learning03 - Encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using Learning03.Assets;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fOne = new Fraction();
+        Console.WriteLine(fOne.GetFractionString());
+        Console.WriteLine(fOne.GetDecimalValue());
+
+        Fraction fTwo = new Fraction(5);
+        Console.WriteLine(fTwo.GetFractionString());
+        Console.WriteLine(fTwo.GetDecimalValue());
+
+        Fraction fThree = new Fraction(3, 4);
+        Console.WriteLine(fThree.GetFractionString());
+        Console.WriteLine(fThree.GetDecimalValue());
+
+        Fraction fFour = new Fraction(1, 3);
+        Console.WriteLine(fFour.GetFractionString());
+        Console.WriteLine(fFour.GetDecimalValue());
+
+
+        Console.WriteLine("\nTesting Getters and Setters");
+        Fraction fFive = new Fraction(2, 9);
+        Console.WriteLine($"GetTop: {fFive.GetTop()}, GetBottom: {fFive.GetBottom()}");
+        Console.WriteLine(fFive.GetFractionString());
+        Console.WriteLine(fFive.GetDecimalValue());
+
+
+        Console.WriteLine("Set top top=3 and bottom=11");
+        fFive.SetTop(3);
+        fFive.SetBottom(11);
+        Console.WriteLine($"GetTop: {fFive.GetTop()}, GetBottom: {fFive.GetBottom()}");
+        Console.WriteLine(fFive.GetFractionString());
+        Console.WriteLine(fFive.GetDecimalValue());
     }
 }
