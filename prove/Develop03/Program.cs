a8b364e36271c9aaf3f4309f4fd41a51ba4aed17
aghogho <aghoghomonorien@gmail.com>
2024-04-22 20:50:00 +0100

    Week 03 Develop - Scripture Memorizer - Working Program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,19 +5,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        // BibleAPI bible = new BibleAPI("John", 3, 16, 19);
-        // Task<string> getBibleText = bible.FetchVersesAsync();
-        // Console.WriteLine(getBibleText.Result);
 
-        Reference refOne = new Reference("John", 3, 16, 19);
-        // string text = refOne.GetDisplayText();
-
-        // Console.WriteLine();
-        // Console.WriteLine(text);
-
-        Scripture script = new Scripture(refOne);
+        Console.Write("What is the name of the Book in the Bible? [For John 3:16, the Book is `John`] ");
+        string book = Console.ReadLine();
+        Console.Write("What is the name of the Chapter? [For John 3:16, the Chapter is `3`] ");
+        int chapter = int.Parse(Console.ReadLine());
+        Console.Write("What is the name of the FromVerse? [For John 3:16-19, the Chapter is `16`] ");
+        int fromVerse = int.Parse(Console.ReadLine());
+        Console.Write("What is the name of the ToVerse (Press Enter if no ToVerse)? [For John 3:16-19, the Chapter is `19`] ");
+        string ToVerseString = Console.ReadLine();
+        int ToVerseInt = (int.TryParse(ToVerseString, out int _)) ?  int.Parse(ToVerseString) : -99;
         
+        Reference refOne = (ToVerseInt != -99) ? new Reference(book, chapter, fromVerse, ToVerseInt) : new Reference(book, chapter, fromVerse);
+        Scripture script = new Scripture(refOne);
 
+        string continuePlay;
+
+        Console.Clear();
+
+        while (!script.IsCompletelyHidden())
+        {
+            Console.WriteLine();
+
+            string toDisplay = $"\n{book} {chapter}:{fromVerse}";
+
+            if (ToVerseInt != -99) toDisplay += $"-{ToVerseInt}";
+            
+            Console.WriteLine($"{toDisplay} {script.GetDisplayText()}");
+
+            Console.Write("\nPress enter to continue or type quit to finish:\n");
+
+            continuePlay = Console.ReadLine();
+
+            if (continuePlay == "")
+            {
+                Console.Write("How many words do you want to hide [press enter to use the default value of 3]? ");
+
+                string numberToHideString = Console.ReadLine();
+
+
+                int numberToHide = (int.TryParse(numberToHideString, out _)) ? int.Parse(numberToHideString) : 3;
+
+                script.HideRandomWords(numberToHide);
+
+                Console.Clear();
+            }
+            else if (continuePlay == "quit") break;
+        }
+        
     }
 }
 
