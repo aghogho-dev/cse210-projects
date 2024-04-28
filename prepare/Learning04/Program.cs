bde4e0a1142977a32c55b514363e93ebb88807de
aghogho <aghoghomonorien@gmail.com>
2024-04-29 00:03:41 +0100

    Learning04 - Inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Learning04.Assets;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+        Assignment assignOne = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignOne.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathOne = new MathAssignment("Roberto Rodrigues", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathOne.GetSummary());
+        Console.WriteLine(mathOne.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writeOne = new WritingAssignment("Mary Waters", "European History", "The Cause of Word War II");
+        Console.WriteLine(writeOne.GetSummary());
+        Console.WriteLine(writeOne.GetWritingInformation());
+        Console.WriteLine();
+        
     }
 }
